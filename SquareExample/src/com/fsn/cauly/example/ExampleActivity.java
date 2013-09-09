8467e3dc42ea1542588410de30f6f8fa76ef494f
윤창주 <yoonc1@futurestream.co.kr>
2013-09-09 17:46:05 +0900

    - 생성

--- SquareExample/src/com/fsn/cauly/example/ExampleActivity.java
+++ SquareExample/src/com/fsn/cauly/example/ExampleActivity.java
@@ -4,49 +4,91 @@ import java.util.ArrayList;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.fsn.cauly.CaulyAdInfo;
 import com.fsn.cauly.CaulyAdInfoBuilder;
 import com.fsn.cauly.CaulySquare;
+import com.fsn.cauly.CaulySquare.CaulySquareActionType;
 import com.fsn.cauly.CaulySquareAd;
 import com.fsn.cauly.CaulySquareListener;
 import com.fsn.cauly.Logger;
 import com.fsn.cauly.Logger.LogLevel;
 
-public class ExampleActivity extends Activity implements CaulySquareListener {
+public class ExampleActivity extends Activity implements CaulySquareListener, OnClickListener {
 
-	
-	// 광고 요청을 위한 App Code
-	private static final String APP_CODE = "CAULY";
-    Button send_offerwall, request_offerlist, show_offerdetail;
+	String APP_CODE="SQUARE_TEST_3";
+    Button show_offerwall, request_offerlist, show_offerdetail,request_adwall_status;
     Button actioncompleted, execcompleted;
     CaulySquare mCaulySquare;
+    ArrayList<CaulySquareAd> mOfferList;
     
 	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        request_adwall_status = (Button) findViewById(R.id.request_adwall_status);
+        show_offerwall = (Button) findViewById(R.id.show_adwall);
+        request_offerlist = (Button) findViewById(R.id.request_ad_data);
+        show_offerdetail = (Button) findViewById(R.id.show_ad_detail);
+        actioncompleted = (Button) findViewById(R.id.send_exec);
+        execcompleted = (Button) findViewById(R.id.send_action);
+        show_offerwall.setOnClickListener(this);
+        request_offerlist.setOnClickListener(this);
+        show_offerdetail.setOnClickListener(this);
+        actioncompleted.setOnClickListener(this);
+        execcompleted.setOnClickListener(this);
+        request_adwall_status.setOnClickListener(this);
+        
+        show_offerwall.setEnabled(false);
+        show_offerdetail.setEnabled(false);
         
         // Cauly 로그 수준 지정 : 로그의 상세함 순서는 다음과 같다.
         //	LogLevel.Info > LogLevel.Warn > LogLevel.Error > LogLevel.None
-        Logger.setLogLevel(LogLevel.Debug);
-     
-        // CaulyAdInfo 생성 : CaulyAdInfoBuilder 사용. APP_CODE 비롯한 전체 설정.
-        // 설정 메소드
-        //    	appCode(String appCode)				: APP_CODE 지정. 생성자 "CaulyAdInfoBuilder(APP_CODE)"를 사용해도 됨.
-
-		CaulyAdInfo adInfo = new CaulyAdInfoBuilder(APP_CODE).build();
-		mCaulySquare = CaulySquare.initWithAdInfo(this, adInfo);
+        Logger.setLogLevel(LogLevel.None);
+       
+        // CaulyAdInfo 생성 : CaulyAdInfoBuilder 사용. APP_CODE 
+		CaulyAdInfo adInfo1 = new CaulyAdInfoBuilder(APP_CODE).build();
 		
-		
+		 // CaulySquare Initiation 
+		mCaulySquare = CaulySquare.initWithAdInfo(this, adInfo1);
+		// kakaoid나 게임에서 쓰는 id를 전달합니다. ( optional ) 
+		mCaulySquare.setCustomId( "kakaoid_of_the_game" );
+		// callback listener register.
+		mCaulySquare.setListener(this);
     }
-
-	
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()) {
+		case R.id.request_adwall_status:
+			mCaulySquare.requestOfferStatus(this);  
+			break;
+		case R.id.show_adwall:
+			mCaulySquare.showOfferwall(this, "타이틀 입니다.");
+			break;
+		case R.id.request_ad_data:
+			mCaulySquare.requestOfferList(this);
+			break;
+		case R.id.show_ad_detail:
+			if(mOfferList.size()>0)
+				mCaulySquare.showOfferDetailDialog(this, mOfferList.get(0));
+			break;
+		case R.id.send_exec:
+			mCaulySquare.reportAction(this, CaulySquareActionType.EXECUTION_COMPLETE, "실행 완료");
+			break;
+		case R.id.send_action:
+			mCaulySquare.reportAction(this, CaulySquareActionType.ACTION_COMPLETE, "액션 완료");
+			break;
+		}
+		
+	}
 	//////////////////////////////
-	//
 	// CaulySquareState Function 호출
-	//
 	//////////////////////////////
 	
 	@Override
@@ -74,46 +116,60 @@ public class ExampleActivity extends Activity implements CaulySquareListener {
 	}
 	
 	//////////////////////////////
-	//
 	// CaulySquare 광고 Listener
-	//
 	//////////////////////////////
 	
-
 	@Override
+	// This is called when Offer Detail Dialogue closed.
 	public void onCloseOfferDetails(int arg0, String arg1) {
-		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
-	public void onCloseOfferwall(int arg0, String arg1) {
-		// TODO Auto-generated method stub
+	// This is called when offerWall closed.
+	public void onCloseOfferwall(int retCode, String retMsg) {
 		
 	}
 
 	@Override
-	public void onOfferListReceived(int arg0, String arg1,
-			ArrayList<CaulySquareAd> arg2) {
-		// TODO Auto-generated method stub
-		
+	// This is called when offer list received. 
+	// CaulySquareAd contains offer Ad information.
+	public void onOfferListReceived(int retCode, String retMsg,	ArrayList<CaulySquareAd> arg2) {
+		if(retCode > 0 )  // success
+		{
+			mOfferList = arg2;
+			if(mOfferList!=null && mOfferList.size()>0)
+				show_offerdetail.setEnabled(true);
+		}
+		else			// failed
+		{
+			Toast.makeText(this, ""+retMsg, Toast.LENGTH_SHORT).show();
+		}
 	}
 
 	@Override
-	public void onOfferStatusReceived(int arg0, String arg1) {
-		// TODO Auto-generated method stub
-		
+	// This is called when offer status received. 
+	// It tells us whether offerwall is available or not;
+	public void onOfferStatusReceived(int retCode, String retMsg) {
+		if(retCode >= 0) // offers is available. 
+		{
+			show_offerwall.setEnabled(true);
+		}
+		else			// offers is not available at this moment for some reason. 
+		{
+			Toast.makeText(this, ""+retMsg, Toast.LENGTH_SHORT).show();
+		}
 	}
 
 	@Override
+	// This is called when Offer Detail Dialogue shows.
 	public void onOpenOfferDetails() {
-		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
+	// This is called when offerwall shows.
 	public void onOpenOfferwall() {
-		// TODO Auto-generated method stub
 		
 	}
 }
